7df6b025d14cec1faedcadae078e81b99bc68944
osamabinshamim <ehteshamhaque355@gmail.com>
2024-11-08 07:54:22 -0800

    Ready for deployment to Platform.sh.

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+'''define Url pattern for accounts.'''
+from django.urls import path, include
+
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    # include default auth urls.
+    path('', include('django.contrib.auth.urls')),
+    # Registration page.
+    path('register/', views.register, name='register'),
+]
